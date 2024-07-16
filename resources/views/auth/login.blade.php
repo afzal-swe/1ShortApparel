c9c059b5271c0f18a010509e704a7ca84793f64c
afzal-swe <afzalbhola07@gmail.com>
2024-07-16 23:54:04 +0600

    comment social section

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -63,14 +63,14 @@
         </div>
       </form>
 
-      <div class="social-auth-links text-center mt-2 mb-3">
+      {{-- <div class="social-auth-links text-center mt-2 mb-3">
         <a href="#" class="btn btn-block btn-primary">
           <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
         </a>
         <a href="#" class="btn btn-block btn-danger">
           <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
         </a>
-      </div>
+      </div> --}}
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
